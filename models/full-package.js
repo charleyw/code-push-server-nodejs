1bb5b4b69a49cb7cab93eb2d42bbff1941e32115
Chao Wang <cwang8023@gmail.com>
2017-01-04 12:09:08 +0800

    fix find package error

--- models/full-package.js
+++ models/full-package.js
@@ -4,7 +4,7 @@ const Deployment = require('./deployment');
 class FullPackage extends AV.Object {
   static findLatestBy(deploymentKey, appVersion) {
     const query = new AV.Query(FullPackage);
-    query.matchesQuery('deployment', AV.Object.createWithoutData('Deployment', deploymentKey));
+    query.equalTo('deployment', AV.Object.createWithoutData('Deployment', deploymentKey));
     query.equalTo('appVersion', appVersion || '');
     query.descending('createdAt');
     return query.first();
